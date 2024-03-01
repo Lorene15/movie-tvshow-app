8c4b0f44aa02cae55609fb31e2b8307362d150ef
Lorene15 <loreneterstena@gmail.com>
2024-03-01 01:06:13 -1100

    main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,25 @@
+import { Button } from "@mui/joy";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div>
+    <div className="container">
+      <Button
+        color="neutral"
+        size="lg"
+        variant="solid"
+        style={{ width: "500px", height: "300px" }}
+      >
         <Link href="/Movie">Movie app</Link>
-      </div>
-      <div>
+      </Button>
+      <Button
+        color="neutral"
+        size="lg"
+        variant="solid"
+        style={{ width: "500px", height: "300px" }}
+      >
         <Link href="/TvShow">TvShow app</Link>
-      </div>
+      </Button>
     </div>
   );
 }
